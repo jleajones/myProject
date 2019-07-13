9a0ea3674a4f5959841efda6a46d186549cb842b
Jason Lea-Jones <jason.leajones@vroom.com>
2019-07-13 18:37:56 -0400

    removed unused fileds

--- src/client/apiPaths.js
+++
@@ -1,4 +0,0 @@
-export const OTHER_PATHS = '';
-const API_HOST = 'http://localhost:3001';
-export const TRACKING_API = `${API_HOST}/api/analytics`;
-export const USERS_API = `${API_HOST}/api/users`;
