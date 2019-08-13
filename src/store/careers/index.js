6cb7f468868eccbff86f7e8b0c4d671c02fd89a2
Jason Lea-Jones <jason.leajones@vroom.com>
2019-08-13 01:00:18 -0400

    remove silly reducer.js file, bad pattern

--- src/store/careers/index.js
+++ src/store/careers/index.js
@@ -1,3 +1,32 @@
-import jobsReducer from './reducers';
+import { Types, FETCH_CAREERS } from './actions/types';
 
-export default jobsReducer;
+const initialState = {
+  list: []
+};
+
+/**
+ *
+ * @param state
+ * @param action
+ * @returns {{list: Array}|({list}&{list})}
+ */
+export default (state = initialState, action = {}) => {
+  const { type, data } = action;
+  switch (type) {
+    case Types[FETCH_CAREERS].PENDING:
+      return {
+        ...state
+      };
+    case Types[FETCH_CAREERS].SUCCESS:
+      return {
+        ...state,
+        list: data
+      };
+    case Types[FETCH_CAREERS].ERROR:
+      return {
+        ...state
+      };
+    default:
+      return state;
+  }
+};
--- src/store/common/index.js
+++ src/store/common/index.js
@@ -1,3 +1,23 @@
-import commentReducer from './reducer';
+import { SET_ERROR } from './actions/types';
 
-export default commentReducer;
+const initialState = {
+  error: ''
+};
+
+/**
+ *
+ * @param state
+ * @param action
+ * @returns {{error: string}|({error}&{error})}
+ */
+export default (state = initialState, action = {}) => {
+  const { type, error } = action;
+  if (type === SET_ERROR) {
+    return {
+      ...state,
+      error
+    };
+  }
+
+  return state;
+};
